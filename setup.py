8d9c194413ae45bb6fa6a9e9010f5a0cb7c950c4
Cristina Muñoz <hi@xmunoz.com>
2021-03-10 08:49:51 -0500

    Clean up requirements, and roll new release

--- setup.py
+++ setup.py
@@ -3,14 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements/requirements-main.txt") as fh:
-    requirements = fh.read().splitlines()
-
-requirements = [f"{r.split('/')[-3]} @ {r}" for r in requirements]
-
 setuptools.setup(
     name="whimsylib",
-    version="0.1.2",
+    version="1.0.0",
     author="The Arch Cronenbrogues",
     author_email="cronenbrogues@googlegroups.com",
     description="A generic game engine for text-based games, inspired by adventurelib.",
